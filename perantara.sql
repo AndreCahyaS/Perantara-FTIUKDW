1307fd8db8b7b69afbe68b49c04b80c416901bdd
FarizalAndrian <farizalandrian@yahoo.co.id>
2013-11-09 16:18:09 +0700

    db

--- perantara.sql
+++ perantara.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 08, 2013 at 07:48 AM
+-- Generation Time: Nov 09, 2013 at 10:17 AM
 -- Server version: 5.5.32
 -- PHP Version: 5.4.19
 
@@ -38,19 +38,25 @@ CREATE TABLE IF NOT EXISTS `topik` (
   `nego` varchar(30) NOT NULL,
   `kondisi` varchar(15) NOT NULL,
   `propinsi` varchar(30) NOT NULL,
-  `tag1` varchar(15) NOT NULL,
-  `tag2` varchar(15) NOT NULL,
-  `tag3` varchar(15) NOT NULL,
-  `tag4` varchar(15) NOT NULL,
+  `tag1` varchar(15) DEFAULT NULL,
+  `tag2` varchar(15) DEFAULT NULL,
+  `tag3` varchar(15) DEFAULT NULL,
+  `tag4` varchar(15) DEFAULT NULL,
+  `status` varchar(30) NOT NULL,
+  `harga` varchar(11) NOT NULL,
   PRIMARY KEY (`id_topik`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data for table `topik`
 --
 
-INSERT INTO `topik` (`id_topik`, `title`, `isi`, `date`, `kategori`, `username`, `nego`, `kondisi`, `propinsi`, `tag1`, `tag2`, `tag3`, `tag4`) VALUES
-(1, 'jual iseng', 'abcdefghijklmn', '2013-11-08 06:41:24', 'elektronik', 'abcdef', 'tidak', 'baru', 'aceh', 'putih', 'biru', 'kuning', 'hijau');
+INSERT INTO `topik` (`id_topik`, `title`, `isi`, `date`, `kategori`, `username`, `nego`, `kondisi`, `propinsi`, `tag1`, `tag2`, `tag3`, `tag4`, `status`, `harga`) VALUES
+(1, 'jual iseng', 'abcdefghijklmn', '2013-11-08 06:41:24', 'elektronik', 'abcdef', 'tidak', 'baru', 'aceh', 'putih', 'biru', 'kuning', 'hijau', '', ''),
+(4, 'aku', 'akasa', '2013-11-09 00:59:45', 'asakskaks', 'skaskak', 'sakska', 'asdsda', 'dsadsa', NULL, NULL, NULL, NULL, 'dsa', 'dsdsas'),
+(5, 'dsaf', 'ssdsads', '2013-11-09 00:59:45', 'fdfdg', 'dsada', 'dsaf', 'dswewa', 'dsdffa', 'dsd', 'fdfd', NULL, NULL, 'sasd2131', 'sdsa'),
+(6, 'dsds', 'dsdsa', '2013-11-09 01:01:10', 'dsa', 'dsa', 'fda', 'dsdsa', 'fdfdg', 'dsds', NULL, NULL, NULL, 'asds23', '321d'),
+(7, '321ds', '321sds', '2013-11-09 01:01:10', '231ssds', '321sdds', 'ds2', 'fdfd', 'sasd', NULL, 'dsds', NULL, 'das', 'ds232', 'fds1');
 
 -- --------------------------------------------------------
 
@@ -68,8 +74,9 @@ CREATE TABLE IF NOT EXISTS `user` (
   `rating` float DEFAULT NULL,
   `avatar` varchar(50) DEFAULT NULL,
   `joindate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `user`
@@ -78,7 +85,8 @@ CREATE TABLE IF NOT EXISTS `user` (
 INSERT INTO `user` (`user_id`, `username`, `nama`, `email`, `password`, `telepon`, `rating`, `avatar`, `joindate`) VALUES
 (1, 'aku', 'aku', 'asa@c.com', 'sasa', NULL, NULL, NULL, '2013-11-04 05:15:26'),
 (2, 'asa', 'sasa', 's@c.com', 'asd', NULL, NULL, NULL, '2013-11-04 05:27:38'),
-(3, 'a', 'aku', 'abc@abc.com', 'asas', NULL, NULL, NULL, '2013-11-07 13:37:40');
+(3, 'a', 'aku', 'abc@abc.com', 'asas', NULL, NULL, NULL, '2013-11-07 13:37:40'),
+(4, 'dsds', 'dsds', 'dsds@ds.com', 'dsds', NULL, NULL, NULL, '2013-11-09 09:15:54');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
