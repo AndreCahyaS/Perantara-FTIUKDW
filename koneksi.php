d0d3c462384ee86095a1718aa8b999535dfdaa3f
Andre Cahya Susanto <deathsworder007@gmail.com>
2013-11-24 20:48:55 +0700

    kelupaan

--- koneksi.php
+++ koneksi.php
@@ -4,11 +4,11 @@
 	$pass = "";
 	$database_name = "u957988429_a";
 	//fungsi konek
-	$koneksi = mysql_connect($host,$user,$pass) or die("gagal konek ke database");
+	$koneksi = mysql_connect($host,$user,$pass) or die(header("location:maintenis.php"));
 
 
 	//Pilih databasenya
-	mysql_select_db($database_name) or die("gagal pilih database");
+	mysql_select_db($database_name) or die(header("location:maintenis.php"));
 
 	
 
